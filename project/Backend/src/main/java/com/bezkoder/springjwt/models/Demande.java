053494f13e26d480bc400f1ec6ec53f6bde859fe
IMANE BOUTATA <imanebt6@gmail.com>
2024-01-04 14:44:28 +0100

    Spring Security + some modifs

---
+++ project/Backend/src/main/java/com/bezkoder/springjwt/models/Demande.java
@@ -0,0 +1,26 @@
+package com.bezkoder.springjwt.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class Demande {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String description;
+	private String type;
+	private Date date;
+	private String status;
+	@ManyToOne
+	User user;
+
+
+}
