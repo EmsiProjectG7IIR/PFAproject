25340111ebb08bcb86544089e09102ec6a7f18df
IMANE BOUTATA <imanebt6@gmail.com>
2024-01-15 12:17:42 +0100

    User

--- project/Backend/src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
+++ project/Backend/src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.bezkoder.springjwt.controllers;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -131,6 +132,58 @@ public class AuthController {
   public ResponseEntity<List<User>> findAllUsers() {
     List<User> users = userRepository.findAll();
     return ResponseEntity.ok(users);
+  }
+  @GetMapping("/findByUsername/{username}")
+  public Optional<User> findByUsername(@PathVariable String username) {
+    return userRepository.findByUsername(username);
+  }
+  @GetMapping("/findById/{id}")
+  public Optional<User> findById(@PathVariable Long id) {
+    return userRepository.findById(id);
+  }
+  @PutMapping("/update/{id}")
+  //@PreAuthorize("hasRole('ADMIN')") // Add appropriate authorization based on your requirements
+  public ResponseEntity<?> updateUser(@PathVariable Long id,  @RequestBody SignupRequest updateUserRequest) {
+    Optional<User> userData = userRepository.findById(id);
+    System.out.println("i am here !!!!!!!!!!!");
+
+    if (userData.isPresent()) {
+      User user = userData.get();
+      user.setUsername(updateUserRequest.getUsername());
+      user.setEmail(updateUserRequest.getEmail());
+      user.setPassword(encoder.encode(updateUserRequest.getPassword())); // Consider validating password updates
+
+      // Update roles if needed
+      Set<String> strRoles = updateUserRequest.getRole();
+      Set<Role> roles = new HashSet<>();
+
+      if (strRoles != null) {
+        strRoles.forEach(role -> {
+          switch (role) {
+            case "admin":
+              Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                      .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+              roles.add(adminRole);
+              break;
+            case "mod":
+              Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                      .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+              roles.add(modRole);
+              break;
+            default:
+              Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                      .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+              roles.add(userRole);
+          }
+        });
+        user.setRoles(roles);
+      }
+
+      userRepository.save(user);
+      return ResponseEntity.ok(new MessageResponse("User updated successfully!"));
+    } else {
+      return ResponseEntity.badRequest().body(new MessageResponse("Error: User not found!"));
+    }
   }
 
 }
--- project/Backend/src/main/java/com/bezkoder/springjwt/repository/UserRepository.java
+++ project/Backend/src/main/java/com/bezkoder/springjwt/repository/UserRepository.java
@@ -10,6 +10,8 @@ import com.bezkoder.springjwt.models.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsername(String username);
+  Optional<User> findById(Long id);
+
 
   Boolean existsByUsername(String username);
 
